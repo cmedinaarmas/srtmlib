1f4ef2b91b9d39342ec0dae760a7b576d33980c5
cmedinaarmas <c.medina.armas@gmail.com>
2018-09-06 14:15:12 -0600

    add new tests

--- script_block.py
+++ script_block.py
@@ -3,10 +3,14 @@ import block as bl
 data = 'data/everest/n27_e085_1arc_v3.bil'
 b = bl.Block(data,1,0)
 # b.load_data()
-data=b.read_bytes(2)
+data=b.read_bytes(3601*3601*2)
+heights= b.decode_hgt(data,False)
+heights_w= b.decode_hgt(data,True)
 #
 # h = b.heights
 # print(b.src_file)
 # print(h.shape)
 # print(b.decoded[0])
-print(data)
+print(data[:2])
+print(heights[0])
+print(heights_w[0])
--- test.py
+++ test.py
@@ -5,16 +5,41 @@ class TestCalc(unittest.TestCase):
 
     def setUp(self):
         src_file = 'data/everest/n27_e085_1arc_v3.bil'
+        self.arc_seconds = 1
         self.b = bl.Block(src_file,1,0)
+
+        self.shape = None
+        if arc == 1:
+            self.shape = (3601,3601)
+        elif arc == 3:
+            self.shape = (1201,1201)
+        else:
+            self.shape = (0,0)
         pass
 
     def trearDown(self):
         pass
 
     def test_read_bytes(self):
-        result = self.b.read_bytes(2)
+        bytes_to_read = 2
+        result = self.b.read_bytes(bytes_to_read)
         self.assertEqual(result[0],117)
         self.assertEqual(result[1],7)
+
+    def test_decode_hgt(self):
+        bytes_to_read = self.shape[0]*self.shape[1]*2
+        data = self.b.read_bytes(bytes_to_read)
+
+        # test default format
+        wrapped = False
+        heights = self.b.decode_hgt(data, wrapped)
+        self.assertEqual(heights[0],1909)
+        # test wrapped format
+        wrapped = True
+        heights = self.b.decode_hgt(data, wrapped)
+        self.assertEqual(heights[0],117)
+
+
 
 if __name__ == '__main__':
     unittest.main()
